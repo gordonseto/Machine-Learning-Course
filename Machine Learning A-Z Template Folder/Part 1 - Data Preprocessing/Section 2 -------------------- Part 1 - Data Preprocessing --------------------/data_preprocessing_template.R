a3e3d8063376a79e13bb70f3e591b1035939bd22
Gordon Seto <gordon.seto@gmail.com>
2017-08-22 14:18:23 -0600

    finish preprocessing section

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -2,21 +2,20 @@
 
 # Import the dataset
 dataset = read.csv('Data.csv')
+# dataset = dataset[, 2:3]
 
-# Take care of missing data
-dataset$Age = ifelse(test = is.na(dataset$Age), 
-                     yes = ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                     no = dataset$Age)
+# Split the dataset into the Training set and Test set
+# install.packages('caTools')
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
 
-dataset$Salary = ifelse(test = is.na(dataset$Salary),
-                        yes = ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                        no = dataset$Salary)
+# Feature scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
 
-# Encode categorical data
-dataset$Country = factor(x = dataset$Country, 
-                         levels = c('France', 'Spain', 'Germany'), 
-                         labels = c(1, 2, 3))
 
-dataset$Purchased = factor(x = dataset$Purchased,
-                           levels = c('No', 'Yes'),
-                           labels = c(0, 1))
+
+
+
