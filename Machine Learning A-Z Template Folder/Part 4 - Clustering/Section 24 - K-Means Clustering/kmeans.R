fc07c14c2d11b3de15afa3b19b307ced0c00ae0a
Gordon Seto <gordon.seto@gmail.com>
2017-08-31 13:18:42 -0600

    finished artificial neural networks in python

---
+++ Machine Learning A-Z Template Folder/Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.R
@@ -0,0 +1,29 @@
+# K-Means Clustering
+
+# Importing the dataset
+dataset = read.csv('Mall_customers.csv')
+X = dataset[4:5]
+
+# Using the Elbow Method to find optimal number of clusters
+set.seed(6)
+wcss = vector()
+for (i in 1:10) wcss[i] = sum(kmeans(X, i)$withinss)
+plot(1:10, wcss, type = 'b', main = paste('Clusters of Clients'), xlab = 'Number of clusters', ylab = 'WCSS')
+
+# Applying K-means to the mall dataset
+set.seed(29)
+kmeans = kmeans(X, 5, iter.max = 300, nstart = 10)
+
+# Visualizing the clusters
+library(cluster)
+clusplot(X, 
+         kmeans$cluster,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Cluster of Clients'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
---
+++ Machine Learning A-Z Template Folder/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
@@ -0,0 +1,83 @@
+# Artificial Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, -1].values
+
+# Encoding categorical data
+# Encoding the Independent Variables
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X1 = LabelEncoder()
+X[:, 1] = labelencoder_X1.fit_transform(X[:, 1])
+labelencoder_X2 = LabelEncoder()
+X[:, 2] = labelencoder_X2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Remove one dummy variable (selecting first column)
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Part 2 - Artificial Neural Network
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initializing the ANN
+classifier = Sequential()
+
+# Adding the input layer and first hidden layer
+# units = number of nodes, rule of thumb: take average of # nodes in output layer and input layer
+# activation = relu, using rectifier function
+# input_dim = number of input nodes. this is required for first layer being added
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_dim=11))
+
+# Adding the second hidden layer
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+
+# Adding the output layer
+classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+
+# Compiling the ANN
+# optimizer = stochastic gradient function, one type is adam
+# loss = the loss function within the stochastic gradient function
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size=10, epochs=100)
+
+# Part 3 - Making the predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
